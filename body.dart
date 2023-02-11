d93baccd6c46e7ada2137e924b369bef6ad7a3e7
diya-mandaliya <125106912+diya-mandaliya@users.noreply.github.com>
2023-02-11 08:40:00 +0530

    Add files via upload

--- body.dart
+++ body.dart
@@ -1,48 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:movie_app/components/genre_card.dart';
 import 'package:movie_app/constants.dart';
 import 'package:movie_app/models/movie.dart';
+import 'dart:math' as math;
 
-import 'backdrop_rating.dart';
-import 'cast_and_crew.dart';
+import 'categories.dart';
 import 'genres.dart';
-import 'title_duration_and_fav_btn.dart';
+import 'movie_card.dart';
+import 'movie_carousel.dart';
 
 class Body extends StatelessWidget {
-  final Movie movie;
-
-  const Body({Key key, this.movie}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // it will provide us total height and width
-    Size size = MediaQuery.of(context).size;
+    // it enable scroll on small device
     return SingleChildScrollView(
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          BackdropAndRating(size: size, movie: movie),
-          SizedBox(height: kDefaultPadding / 2),
-          TitleDurationAndFabBtn(movie: movie),
-          Genres(movie: movie),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: kDefaultPadding / 2,
-              horizontal: kDefaultPadding,
-            ),
-            child: Text(
-              "Plot Summary",
-              style: Theme.of(context).textTheme.headline5,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-            child: Text(
-              movie.plot,
-              style: TextStyle(
-                color: Color(0xFF737599),
-              ),
-            ),
-          ),
-          CastAndCrew(casts: movie.cast),
+          Categorylist(),
+          Genres(),
+          SizedBox(height: kDefaultPadding),
+          MovieCarousel(),
         ],
       ),
     );
--- genres.dart
+++ genres.dart
@@ -1,30 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/components/genre_card.dart';
-import 'package:movie_app/models/movie.dart';
 
 import '../../../constants.dart';
 
 class Genres extends StatelessWidget {
-  const Genres({
-    Key key,
-    @required this.movie,
-  }) : super(key: key);
-
-  final Movie movie;
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
-      child: SizedBox(
-        height: 36,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: movie.genra.length,
-          itemBuilder: (context, index) => GenreCard(
-            genre: movie.genra[index],
-          ),
-        ),
+    List<String> genres = [
+      "Action",
+      "Crime",
+      "Comedy",
+      "Drama",
+      "Horror",
+      "Animation"
+    ];
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+      height: 36,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: genres.length,
+        itemBuilder: (context, index) => GenreCard(genre: genres[index]),
       ),
     );
   }
